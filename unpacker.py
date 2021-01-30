8eda02d9cee1bcb7290c702a15b0a047dd53b7d2
Maximilian Fischer <maximilian@fischers-home.com>
2021-01-30 18:07:43 +0100

    Added support for '~' sign in archive path

--- unpacker.py
+++ unpacker.py
@@ -1,4 +1,5 @@
 import patoolib
+import os
 
 def unpack(path, todir=None):
     """Unpack the given file
@@ -7,7 +8,9 @@ def unpack(path, todir=None):
     path -- the path to the file which to unpack
     todir -- the location to which to unpack the file
     """
+    path = os.path.expanduser(path)
     if todir:
+        todir = os.path.expanduser(todir)
         patoolib.extract_archive(path, outdir=todir)
     else:
         patoolib.extract_archive(path)
