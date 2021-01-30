959f379965284c81ce6348acf93c7049c9b261e7
Maximilian Fischer <maximilian@fischers-home.com>
2021-01-30 23:11:11 +0100

    Added ListStep

---
+++ steps/liststep.py
@@ -0,0 +1,48 @@
+from .step import Step
+from exceptions import ConfigValueError
+import todolist
+
+
+class ListStep(Step):
+    """A step that is another todolist
+
+    This step shouldn't be instanciated, since the fromjson method doesn't
+    actually return a ListStep object, but a Todolist object, since both use
+    the execute funtions to execute, this class is mainly there if this has to
+    be changed in the future, and for continuity.
+
+    class methods:
+    fromjson() -- return a todolist object
+    """
+
+    def fromjson(stepjson, listjson):
+        """Return a todolist object from a liststep json
+
+        This doesn't actually return a ListStep object, but a Todolist object,
+        since the Todolist object is very similar to this object.
+        This method doesn't check for circular todolist calls, so be careful
+        with that.
+
+        arguments:
+        stepjson -- the json of the whole step already imported into python
+        listjson -- the json of the whole list file the step was in
+
+        exceptions:
+        KeyError -- if a needed attribute in the json is not found
+                    this error contains an attribute \"message\", which
+                    contains the errormessage
+        """
+        try:
+            command = stepjson['command']
+        except KeyError as e:
+            e.message = 'Missing command attribute in ListStep'
+            raise
+        path = command.get('path', None)
+        name = command.get('name', None)
+        if path:
+            return todolist.TodoList.fromfile(path, name)
+        elif name:
+            return todolist.TodoList.fromjson(listjson, name)
+        else:
+            message = 'Found neither path nor name in todolist step'
+            raise ConfigValueError(message)
--- steps/stepmap.py
+++ steps/stepmap.py
@@ -3,9 +3,11 @@ from .executestep import ExecuteStep
 from .downloadstep import DownloadStep
 from .unpackstep import UnpackStep
 from .gitstep import GitStep
+from .liststep import ListStep
 
 stepmap = {'collection': CollectionStep.fromjson,
            'execute': ExecuteStep.fromjson,
            'download': DownloadStep.fromjson,
            'unpack': UnpackStep.fromjson,
-           'git': GitStep.fromjson}
+           'git': GitStep.fromjson,
+           'list': ListStep.fromjson}
