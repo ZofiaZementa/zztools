88b774050d1431762a76cf0ec09d4a02c65e83f9
Maximilian Fischer <maximilian@fischers-home.com>
2021-02-09 21:42:14 +0100

    Implemented ovirrding of sudo

--- utilities/executor.py
+++ utilities/executor.py
@@ -1,15 +1,74 @@
+import types
 import sys
 import subprocess
 
 
-def execute_command(command, sudo=False):
-    """Executes the given command, with sudo of the given value
+class Executor(types.ModuleType):
 
-    arguments:
-    command -- the command which to execute
-    sudo -- whether or not to use sudo (default: False)
-    """
-    command = command.split(' ')
-    if sudo:
-        command = ['sudo'] + command
-    subprocess.run(command, stderr=sys.stderr)
+    _override_sudo = None
+
+    def is_sudo_overridden(self) -> bool:
+        """returns whether sudo is overridden or not"""
+        return self._override_sudo is not None
+
+    @property
+    def override_sudo(self):
+        """Return the actual value of _override_sudo"""
+        return self._override_sudo
+
+    @override_sudo.setter
+    def override_sudo(self, sudo):
+        """override sudo
+
+        overrides the sudo value given in execute_command with the vaule given
+        to this function
+
+        arguments:
+        sudo -- what to override sudo with
+
+        exceptions:
+        TypeError -- if the given object does not implement bool
+        """
+        self._override_sudo = sudo
+
+    @override_sudo.getter
+    def override_sudo(self) -> bool:
+        """returns the vaule of _override_sudo
+
+        exceptions:
+        AttributeError -- if override_sudo is not set
+        """
+        if self.is_sudo_overridden():
+            if self._override_sudo:
+                return True
+            else:
+                return False
+        else:
+            message = 'Attribute override_sudo is no set'
+            raise AttributeError(message)
+
+    @override_sudo.deleter
+    def override_sudo(self):
+        """deletes the current value of _override_sudo"""
+        self._override_sudo = None
+
+    def execute_command(self, command: str, sudo=False):
+        """Executes the given command, with sudo of the given value
+
+        Warning: this functions behavior depends on the value of the override_sudo
+        property of this module
+
+        arguments:
+        command -- the command which to execute
+        sudo -- whether or not to use sudo (default: False)
+        """
+        if self.is_sudo_overridden():
+            sudo = self.override_sudo
+        command = command.split(' ')
+        if sudo:
+            command = ['sudo'] + command
+        subprocess.run(command, stderr=sys.stderr)
+
+
+if __name__ != '__main__':
+    sys.modules[__name__] = Executor(__name__)
--- zztools.py
+++ zztools.py
@@ -8,6 +8,7 @@ from packagemanager import PackageManager
 from collection import Collection
 import pseudopackages
 from exceptions import ConfigValueError, UnsupportedFileTypeError
+import utilities.executor
 
 
 def _collectionsfromargs(args):
@@ -97,43 +98,45 @@ def _parser():
             action='store_false', \
             help='override the sudo value for all packagemanager', \
             dest='sudo')
-    packagepanager_parser = argparse.ArgumentParser(add_help=False)
-    packagepanager_parser.add_argument('-c', '--collection-file', \
+    packagemanager_parser = argparse.ArgumentParser(add_help=False)
+    packagemanager_parser.add_argument('-c', '--collection-file', \
             required=True, \
             help='specifies the file with the collections', \
             metavar='FILE', \
             dest='collection_file')
-    packagepanager_parser.add_argument('-m', '--packagemanager-file', \
+    packagemanager_parser.add_argument('-m', '--packagemanager-file', \
             required=True, \
             help='specifies the file with the packagemanagers', \
             metavar='FILE', \
             dest='packagemanager_file')
-    packagepanager_parser.add_argument('-p', '--package-file', \
+    packagemanager_parser.add_argument('-p', '--package-file', \
             required=True, \
             help='specifies the file with the packages', \
             metavar='FILE', \
             dest='pseudopackages_file')
-    packagepanager_parser.add_argument('-g', '--packagemanager-list', \
+    packagemanager_parser.add_argument('-g', '--packagemanager-list', \
             nargs='+', \
             help='use only the packagemanagers in the order given in the list', \
             metavar='PACKAGEMANAGER', \
             dest='packagemanagers')
-    packagepanager_parser.add_argument('-l', '--collection-list', \
+    packagemanager_parser.add_argument('-l', '--collection-list', \
             nargs='+', \
             help='use only the collections given in the list', \
             metavar='COLLECTION', \
             dest='collections')
     parser_action_install = subparser_action.add_parser('install', \
             description='Installs the given collections', \
-            parents=[packagepanager_parser])
+            parents=[packagemanager_parser])
     parser_action_uninstall = subparser_action.add_parser('uninstall', \
             description='Uninstalls the given collections', \
-            parents=[packagepanager_parser])
+            parents=[packagemanager_parser])
     return parser
 
 
 def _main():
     args = _parser().parse_args()
+    if args.sudo is not None:
+        utilities.executor.override_sudo = args.sudo
     function = _commandfunctionmapping(args.action)
     if not function:
         _parser().print_usage()
